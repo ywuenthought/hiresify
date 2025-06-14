a5af0e564d6ac500e37888c03bbb4fcd12ac75f7
ywu <yifengwucms@gmail.com>
2025-06-13 23:52:29 -0500

    DEV: Add initial implementations of user-related repository methods

--- engine/src/hiresify_engine/db/repository.py
+++ engine/src/hiresify_engine/db/repository.py
@@ -12,11 +12,12 @@ import json
 import typing as ty
 from collections import abc
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from hiresify_engine.type import FilePath
 
-from .models import Base
+from .models import Base, UserAuth
 
 
 class Repository:
@@ -58,3 +59,66 @@ class Repository:
         Dispose of the database engine and close all pooled connections.
         """
         await self._engine.dispose()
+
+    #################
+    # user management
+    #################
+
+    async def find_user(self, username: str) -> UserAuth | None:
+        """
+        Find the user with the given user name.
+        """
+        where_clause = UserAuth.username == username
+
+        async with self.session() as session:
+            stmt = select(UserAuth).where(where_clause)
+            result = await session.execute(stmt)
+
+            return result.scalar_one_or_none()
+
+    async def register_user(self, username: str, password: str) -> UserAuth:
+        """
+        Register a user given a user name and a hashed password.
+        """
+        user = UserAuth(username=username, password=password)
+
+        async with self.session() as session:
+            async with session.begin():
+                session.add(user)
+
+            await session.refresh(user)
+            return user
+
+    async def update_password(self, username: str, password: str) -> bool:
+        """
+        Update a user's password given the user name and the new hashed password.
+        """
+        where_clause = UserAuth.username == username
+
+        async with self.session() as session:
+            async with session.begin():
+                stmt = select(UserAuth).where(where_clause)
+                result = await session.execute(stmt)
+
+                if user := result.scalar_one_or_none():
+                    user.password = password
+                    return True
+
+                return False
+
+    async def delete_user(self, username: str) -> bool:
+        """
+        Delete a user by the given user name.
+        """
+        where_clause = UserAuth.username == username
+
+        async with self.session() as session:
+            async with session.begin():
+                stmt = select(UserAuth).where(where_clause)
+                result = await session.execute(stmt)
+
+                if user := result.scalar_one_or_none():
+                    await session.delete(user)
+                    return True
+
+                return False
