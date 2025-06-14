b3f9009fcba018701de196b15dd65215d140a91d
ywu <yifengwucms@gmail.com>
2025-06-13 22:24:58 -0500

    DEV: Add the base methods of Repository

--- engine/src/hiresify_engine/db/repository.py
+++ engine/src/hiresify_engine/db/repository.py
@@ -7,12 +7,16 @@
 Exports the repository layer around the database.
 """
 
+import contextlib
 import json
 import typing as ty
+from collections import abc
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from hiresify_engine.type import FilePath
+
+from .models import Base
 
 
 class Repository:
@@ -33,3 +37,24 @@ class Repository:
             configs = json.load(fp)
 
         return cls(url, **configs)
+
+    @contextlib.asynccontextmanager
+    async def session(self) -> abc.AsyncGenerator[AsyncSession, None]:
+        """
+        Provide an async context-managed database session.
+        """
+        async with self._create_session() as session:
+            yield session
+
+    async def init_schema(self) -> None:
+        """
+        Initialize all the tables based on a pre-defined schema.
+        """
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async def dispose(self) -> None:
+        """
+        Dispose of the database engine and close all pooled connections.
+        """
+        await self._engine.dispose()
