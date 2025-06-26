dd06a8b55245affa1c88d2a2c17a8f2c0570c64a
ywu <yifengwucms@gmail.com>
2025-06-25 20:29:10 -0500

    CLN: Remove the `login_user_page` endpoint (moving to frontend)

--- engine/src/hiresify_engine/templates/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025 Yifeng Wu
-# All rights reserved.
-# This file is not licensed for use, modification, or distribution without
-# explicit written permission from the copyright holder.
-
-from .path import LOGIN_HTML_PATH as LOGIN_HTML
-
-__all__ = ["LOGIN_HTML"]
