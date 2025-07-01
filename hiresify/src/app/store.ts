4a27f552d04162f32f4263a382dff3dd4a054506
ywu <yifengwucms@gmail.com>
2025-06-30 21:53:36 -0500

    DEV: Add more boilerplate types for the app's redux

--- hiresify/src/app/store.ts
+++ hiresify/src/app/store.ts
@@ -3,15 +3,39 @@
 // Access, use, and distribution is subject to written agreement
 // by and between Enthought, Inc. and Daiichi Sankyo.
 
-import { configureStore } from '@reduxjs/toolkit';
+import type {
+  Action,
+  ThunkAction,
+  TypedStartListening,
+} from '@reduxjs/toolkit';
+import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
 
 import rootReducer from './rootReducer';
 
+export const listenerMiddleware = createListenerMiddleware();
+
 export type RootState = ReturnType<typeof rootReducer>;
 
-const store = configureStore({ reducer: rootReducer });
+// The store setup is wrapped in `makeStore` to allow reuse when setting up tests that
+// need the same store config.
+export const makeStore = (preloadedState?: Partial<RootState>) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().prepend(listenerMiddleware.middleware),
+    preloadedState,
+  });
 
-export type AppStore = typeof store;
+  return store;
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
 export type AppDispatch = AppStore['dispatch'];
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>;
 
-export default store;
+export type AppStartListening = TypedStartListening<RootState, AppDispatch>;
