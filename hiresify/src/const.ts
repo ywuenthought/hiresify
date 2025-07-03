37c3a9156386c6a74b8f637b6f9716ed6ceaafb6
ywu <yifengwucms@gmail.com>
2025-07-03 01:11:07 -0500

    DEV: Dynamically get the callback URL

--- hiresify/src/const.ts
+++ hiresify/src/const.ts
@@ -6,23 +6,16 @@ const {
   VITE_API_SCHEME: API_SCHEME,
   VITE_API_HOST: API_HOST,
   VITE_API_PORT: API_PORT,
-
-  VITE_APP_SCHEME: APP_SCHEME,
-  VITE_APP_HOST: APP_HOST,
-  VITE_APP_PORT: APP_PORT,
 } = import.meta.env;
 
 const API_PREFIX = `${API_SCHEME}://${API_HOST}:${API_PORT}`;
-const APP_PREFIX = `${APP_SCHEME}://${APP_HOST}:${APP_PORT}`;
-
 const TOKEN_PREFIX = `${API_PREFIX}/token`;
+const USER_PREFIX = `${API_PREFIX}/user`;
 
 export const tokenUrls = {
   issue: `${TOKEN_PREFIX}/issue`,
   revoke: `${TOKEN_PREFIX}/revoke`,
 };
-
-const USER_PREFIX = `${API_PREFIX}/user`;
 
 export const userUrls = {
   authorize: `${USER_PREFIX}/authorize`,
@@ -34,7 +27,7 @@ export const routes = {
     children: {
       callback: { root: '/callback', children: {} },
     },
-    root: '/',
+    root: '',
   },
   main: {
     children: {},
@@ -43,6 +36,6 @@ export const routes = {
 };
 
 export const CALLBACK_URL =
-  `${APP_PREFIX}/#` +
+  `${window.location.origin}` +
   `${routes.home.root}` +
   `${routes.home.children.callback.root}`;
