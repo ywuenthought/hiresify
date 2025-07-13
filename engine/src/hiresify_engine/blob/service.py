7aa23c49d8e5696afa046c31548e5043b59b2ee1
ywu <yifengwucms@gmail.com>
2025-07-13 01:50:41 -0500

    DEV: Add an initial implementation of `BlobService`

---
+++ engine/src/hiresify_engine/blob/service.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2025 Yifeng Wu
+# All rights reserved.
+# This file is not licensed for use, modification, or distribution without
+# explicit written permission from the copyright holder.
+
+"""Export the blob service layer for managing media files."""
+
+import typing as ty
+from contextlib import asynccontextmanager
+from itertools import count
+
+from aioboto3 import Session
+from aiobotocore.config import AioConfig
+from types_aiobotocore_s3.client import S3Client
+
+from hiresify_engine.envvar import (
+    BLOB_ACCESS_KEY,
+    BLOB_SECRET_KEY,
+    BLOB_STORE_REGION,
+    BLOB_STORE_URL,
+    BUCKET_NAME,
+    PRODUCTION,
+)
+
+from .type import Uploader
+
+
+class BlobService:
+    """A wrapper class providing APIs to manage the blob store."""
+
+    def __init__(self) -> None:
+        """Initialize a new instance of BlobService."""
+        self._session = Session()
+        self._config = AioConfig(
+            retries=dict(max_attempts=3),
+            s3=dict(addressing_style="auto" if PRODUCTION else "path"),
+            signature_version="s3v4",
+        )
+
+    def _create_client(self) -> ty.AsyncContextManager[S3Client]:
+        """Create an instance of S3 client as an async context manager."""
+        return self._session.client(
+            "s3",
+            aws_access_key_id=BLOB_ACCESS_KEY,
+            aws_secret_access_key=BLOB_SECRET_KEY,
+            config=self._config,
+            endpoint_url=BLOB_STORE_URL,
+            region_name=BLOB_STORE_REGION,
+            use_ssl=PRODUCTION,
+        )
+
+    @asynccontextmanager
+    async def upload_by_parts(self, blob_key: str) -> ty.AsyncGenerator[Uploader, None]:
+        """Upload a media (image or video) file to the blob store by parts."""
+        parts: list[tuple[int, str]] = []
+        index = count(1)
+        upload_id = None
+
+        async with self._create_client() as client:
+            try:
+                ctx = await client.create_multipart_upload(
+                    Bucket=BUCKET_NAME,
+                    Key=blob_key,
+                )
+                upload_id = ctx["UploadId"]
+
+                async def uploader(body: bytes) -> None:
+                    part_index = next(index)
+                    resp = await client.upload_part(
+                        Body=body,
+                        Bucket=BUCKET_NAME,
+                        Key=blob_key,
+                        PartNumber=part_index,
+                        UploadId=upload_id,
+                    )
+                    parts.append((part_index, resp["ETag"]))
+
+                yield uploader
+
+                parts.sort(key=lambda part: part[0])
+                await client.complete_multipart_upload(
+                    Bucket=BUCKET_NAME,
+                    Key=blob_key,
+                    MultipartUpload=dict(
+                        Parts=[
+                            dict(ETag=etag, PartNumber=part_index)
+                            for part_index, etag
+                            in parts
+                        ],
+                    ),
+                    UploadId=upload_id,
+                )
+
+            except Exception as upload_error:
+                msg = f"The upload of media file {blob_key} failed."
+
+                if upload_id:
+                    try:
+                        await client.abort_multipart_upload(
+                            Bucket=BUCKET_NAME,
+                            Key=blob_key,
+                            UploadId=upload_id,
+                        )
+                    except Exception as abort_error:
+                        raise ExceptionGroup(
+                            f"{msg} Aborting it also failed.",
+                            [upload_error, abort_error],
+                        ) from None
+
+                raise RuntimeError(msg) from upload_error
--- engine/src/hiresify_engine/envvar.py
+++ engine/src/hiresify_engine/envvar.py
@@ -10,6 +10,21 @@ from .util import get_envvar
 # The access token TTL (sec).
 ACCESS_TTL = get_envvar("ACCESS_TTL", int, 900)
 
+# The access key of the blob store.
+BLOB_ACCESS_KEY = get_envvar("BLOB_ACCESS_KEY", str, "")
+
+# The secret key of the blob store.
+BLOB_SECRET_KEY = get_envvar("BLOB_ACCESS_KEY", str, "")
+
+# The region name of the blob store.
+BLOB_STORE_REGION = get_envvar("BLOB_ACCESS_KEY", str, "")
+
+# The endpoint URL of the blob store.
+BLOB_STORE_URL = get_envvar("BLOB_ACCESS_KEY", str, "")
+
+# The name of the bucket for blob storage.
+BUCKET_NAME = get_envvar("BUCKET_NAME", str, "hiresify")
+
 # The TTL for a cache entry (sec).
 CACHE_TTL = get_envvar("CACHE_TTL", int, 300)
 
