f9cde7027dc1eba5fb516856d4c802706dc77cf0
ywu <yifengwucms@gmail.com>
2025-07-24 23:01:46 -0500

    DEV: Reimplement `UploadMemoryStore`

--- hiresify/src/upload/store.ts
+++ hiresify/src/upload/store.ts
@@ -2,64 +2,96 @@
 // This file is part of incredible-me and is licensed under the MIT License.
 // See the LICENSE file for more details.
 
+import Denque from 'denque';
+
 import { CHUNK_SIZE } from '@/const';
 
-import type { PartMeta } from './type';
+import type { PartMeta, Status } from './type';
 
 export default class UploadMemoryStore {
-  private toSendMap: Map<string, PartMeta[]> = new Map();
-  private passedMap: Map<string, PartMeta[]> = new Map();
-  private pausedMap: Map<string, PartMeta[]> = new Map();
-  private failedMap: Map<string, PartMeta[]> = new Map();
+  private failedParts: Denque<PartMeta> = new Denque();
+  private onDutyParts: Set<PartMeta> = new Set();
+  private passedParts: Denque<PartMeta> = new Denque();
+  private pausedParts: PartMeta[] = [];
+  private toSendParts: Denque<PartMeta> = new Denque();
 
-  public failPart(args: { uploadId: string; partMeta: PartMeta }): void {
-    const { uploadId, partMeta } = args;
-    const parts = this.failedMap.get(uploadId);
-    parts?.push(partMeta);
-  }
-
-  public nextPart(args: { uploadId: string }): PartMeta | undefined {
-    const { uploadId } = args;
-    const parts = this.toSendMap.get(uploadId);
-    return parts?.pop();
-  }
-
-  public passPart(args: { uploadId: string; partMeta: PartMeta }): void {
-    const { uploadId, partMeta } = args;
-    const parts = this.passedMap.get(uploadId);
-    parts?.push(partMeta);
-  }
-
-  public pause(args: { uploadId: string }): void {
-    const { uploadId } = args;
-    const parts = this.toSendMap.get(uploadId);
-
-    if (parts) {
-      this.pausedMap.set(uploadId, parts);
-    }
-  }
-
-  public resume(args: { uploadId: string }): void {
-    const { uploadId } = args;
-    const parts = this.pausedMap.get(uploadId);
-
-    if (parts) {
-      this.toSendMap.set(uploadId, parts);
-    }
-  }
-
-  public start(args: { uploadId: string; file: File }): void {
-    const { uploadId, file } = args;
+  constructor(args: { file: File }) {
+    const { file } = args;
 
     const count = Math.ceil(file.size / CHUNK_SIZE);
-    const parts: PartMeta[] = Array.from({ length: count }, (_, i) => {
-      const offset = i * CHUNK_SIZE;
-      return {
-        index: i + 1,
+    for (let index = 1; index <= count; index += 1) {
+      const offset = (index - 1) * CHUNK_SIZE;
+      this.toSendParts.push({
+        index: index,
         bound: [offset, Math.min(offset + CHUNK_SIZE, file.size)],
-      };
-    });
+      });
+    }
+  }
 
-    this.toSendMap.set(uploadId, parts.reverse());
+  public failPart(args: { part: PartMeta }): void {
+    const { part } = args;
+
+    if (!this.onDutyParts.has(part)) {
+      return;
+    }
+
+    this.onDutyParts.delete(part);
+    this.failedParts.push(part);
+  }
+
+  public nextPart(): PartMeta | undefined {
+    const part = this.toSendParts.shift();
+
+    if (part) {
+      this.onDutyParts.add(part);
+    }
+
+    return part;
+  }
+
+  public passPart(args: { part: PartMeta }): void {
+    const { part } = args;
+
+    if (!this.onDutyParts.has(part)) {
+      return;
+    }
+
+    this.onDutyParts.delete(part);
+    this.passedParts.push(part);
+  }
+
+  public pause(): void {
+    while (this.toSendParts.length > 0) {
+      this.pausedParts.push(this.toSendParts.pop() as PartMeta);
+    }
+  }
+
+  public resume(): void {
+    while (this.pausedParts.length > 0) {
+      this.toSendParts.push(this.pausedParts.pop() as PartMeta);
+    }
+  }
+
+  public retry(): void {
+    while (this.failedParts.length > 0) {
+      this.toSendParts.push(this.failedParts.shift() as PartMeta);
+    }
+  }
+
+  public status(): Status {
+    const failed = this.failedParts.length;
+    const onDuty = this.onDutyParts.size;
+    const passed = this.passedParts.length;
+    const paused = this.pausedParts.length;
+    const toSend = this.toSendParts.length;
+    const progress = passed / (failed + onDuty + passed + paused + toSend);
+    return {
+      failed,
+      onDuty,
+      passed,
+      paused,
+      toSend,
+      progress,
+    };
   }
 }
--- hiresify/src/upload/type.ts
+++ hiresify/src/upload/type.ts
@@ -4,8 +4,17 @@
 
 export type PartMeta = {
   // The index of this part.
-  index: number;
-
+  readonly index: number;
   // The starting and ending byte positions of this part in the parent file.
-  bound: [number, number];
+  readonly bound: [number, number];
+};
+
+export type Status = {
+  readonly failed: number;
+  readonly onDuty: number;
+  readonly passed: number;
+  readonly paused: number;
+  readonly toSend: number;
+  // passed divided by total, a number between 0 and 1.
+  readonly progress: number;
 };
