ef003293b433dffd410f9616f8873141ae4eebb9
ywu <yifengwucms@gmail.com>
2025-07-26 03:30:19 -0500

    DEV: Implement `UploadQueue`

---
+++ hiresify/src/upload/queue.ts
@@ -0,0 +1,41 @@
+// Copyright (c) 2025 Yifeng Wu
+// This file is part of incredible-me and is licensed under the MIT License.
+// See the LICENSE file for more details.
+
+import Denque from 'denque';
+
+type Job = () => Promise<void>;
+
+export default class UploadQueue {
+  private concurrency: number = 1;
+  private runningJobs: number = 0;
+  private pendingJobs: Denque<Job> = new Denque();
+
+  constructor(args: { concurrency?: number }) {
+    const { concurrency = 1 } = args;
+
+    if (concurrency < 1 || !Number.isInteger(concurrency)) {
+      throw new Error('Invalid pool concurrency was given.');
+    }
+
+    this.concurrency = concurrency;
+  }
+
+  public enqueue(args: { job: Job }) {
+    const { job } = args;
+
+    this.pendingJobs.push(job);
+    this.runNextJobs();
+  }
+
+  private runNextJobs() {
+    while (this.runningJobs < this.concurrency && this.pendingJobs.length > 0) {
+      const job = this.pendingJobs.shift() as Job;
+      this.runningJobs += 1;
+      job().finally(() => {
+        this.runningJobs -= 1;
+        queueMicrotask(() => this.runNextJobs());
+      });
+    }
+  }
+}
