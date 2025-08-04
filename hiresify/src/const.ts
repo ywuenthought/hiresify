ee38957be811d6a5d6188ba32e10acadcef80bc6
ywu <yifengwucms@gmail.com>
2025-08-04 03:48:53 -0500

    STYLE: Rename the envvar for upload concurrency

--- hiresify/src/const.ts
+++ hiresify/src/const.ts
@@ -2,9 +2,9 @@
 // This file is part of incredible-me and is licensed under the MIT License.
 // See the LICENSE file for more details.
 
-const { VITE_CHUNK_SIZE = '4', VITE_CONCURRENCY = '3' } = import.meta.env;
+const { VITE_CHUNK_SIZE, VITE_MAX_JOBNUM } = import.meta.env;
 
 // Convert the chunk size from mb to bytes.
 export const CHUNK_SIZE = 1024 ** 2 * Number(VITE_CHUNK_SIZE);
 
-export const UPLOAD_CONCURRENCY = Number(VITE_CONCURRENCY);
+export const UPLOAD_CONCURRENCY = Number(VITE_MAX_JOBNUM);
